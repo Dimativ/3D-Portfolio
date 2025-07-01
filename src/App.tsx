d96b3677ef74848d1b6a1b149313f685642377f5
Dimativ <88792228+Dimativ@users.noreply.github.com>
2025-07-01 16:01:06 +0300

    feature/contact (#9)
    feat: implemented contact section

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Navbar } from './components/Navbar.tsx';
+import { Contact } from './sections/Contact.tsx';
 import { ExperienceSection } from './sections/ExperienceSection.tsx';
 import { FeatureCards } from './sections/FeatureCards.tsx';
 import { Hero } from './sections/Hero.tsx';
@@ -9,15 +10,14 @@ import { Testimonials } from './sections/Testimonials.tsx';
 const App = () => {
   return (
     <main>
-      <div>
-        <Navbar />
-        <Hero />
-        <ShowcaseSection />
-        <FeatureCards />
-        <ExperienceSection />
-        <TechStack />
-        <Testimonials />
-      </div>
+      <Navbar />
+      <Hero />
+      <ShowcaseSection />
+      <FeatureCards />
+      <ExperienceSection />
+      <TechStack />
+      <Testimonials />
+      <Contact />
     </main>
   );
 };
